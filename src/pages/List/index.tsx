a6f7375bd8b9e5d1c34eb9bbde1498afedbb046e
Gabriela Martins <gabriela.martins969@gmail.com>
2022-02-08 17:08:15 -0300

    listagem

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,19 +1,48 @@
 import React from "react";
-import { Container } from "./styles"
+import { Container, Content, Filters } from "./styles"
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
-    const options = [
-        {value:'Gabriela', label:'Gabriela'},
-        {value:'Gabriela', label:'Gabriela'},
-        {value:'Gabriela', label:'Gabriela'},
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
+    ];
+    const years = [
+        { value: 2020, label: '2020' },
+        { value: 2019, label: '2019' },
+        { value: 2018, label: '2018' },
     ];
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E">
-                <SelectInput options={options}/>
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+            <Filters>
+                <button 
+                    type="button"
+                    className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button 
+                    type="button"
+                    className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filters>
+            <Content>
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de Luz"
+                    subtitle="27/07/2020"
+                    amount="R$130,00"
+                />
+            </Content>
         </Container>
     );
 }
